ee3fca66ae63874df4bf6df96ba6d5fd03ece35c
Brendan <brendanathoele@gmail.com>
2025-12-18 08:04:45 -0600

    Add basic argparse command logic

--- cli_tasklist.py
+++ cli_tasklist.py
@@ -1,32 +1,17 @@
-print("Welcome to the cli task list!")
-id_increment = 0
+import argparse
 
-def add(task_name):
-    global id_increment
-    id_increment += 1
+FILENAME = "todo.txt"
 
-    task = {
-        "id": id_increment,
-        "description": task_name,
-        "status": "todo"
-    }
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Manage your to-do list")
+    parser.add_argument("action", choices=["add", "list", "done"], help="Action to perform")
+    parser.add_argument("--item-text", help="Text of the item to add")
+    parser.add_argument("--item-number", type=int, help="Number of the item to mark done")
+    args = parser.parse_args()
 
-    print(f"{task["id"]} {task["description"]} ({task["status"]})")
-
-
-
-add(input("Please enter your first task: "))
-
-def main():
-    while True:
-        command = input("Enter a command: ")
-
-        if command.split(" ")[0] == "add":
-            new_task_name = command.split(" ")[1]
-            add(new_task_name)
-
-        if command == "exit":
-            print("exiting")
-            break
-
-main()
+    if args.action == "add":
+        # add_item(args.item_text)
+    elif args.action == "done":
+        # mark_item_done(args.item_number)
+    elif args.action == "list":
+        # list_items()
